0e3e74d7436631839fc58f9485d57bdfcd4d1100
Iryna Kavalenka <iryna_kavalenka@epam.com>
2023-11-19 18:35:15 +0100

    refactor: move lambda folder inside products lib item

--- lib/products/products-stack.ts
+++ lib/products/products-stack.ts
@@ -1,13 +1,13 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 
 export class ProductsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const api = new apigw.RestApi(this, 'Products API', {
+    const api = new apigw.RestApi(this, "Products API", {
       restApiName: "Products",
       defaultCorsPreflightOptions: {
         allowHeaders: ["*"],
@@ -16,20 +16,34 @@ export class ProductsStack extends cdk.Stack {
       },
     });
 
-    const products = api.root.addResource('products', );
-    const productsLambdaIntegration = new lambda.Function(this, 'All Products Endpoint', {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'products.getProductsList',
-    });
-    products.addMethod(lambda.HttpMethod.GET, new apigw.LambdaIntegration(productsLambdaIntegration));
+    const products = api.root.addResource("products");
+    const productsLambdaIntegration = new lambda.Function(
+      this,
+      "All Products Endpoint",
+      {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        code: lambda.Code.fromAsset("lib/products"),
+        handler: "lambda.getProductsList",
+      }
+    );
+    products.addMethod(
+      lambda.HttpMethod.GET,
+      new apigw.LambdaIntegration(productsLambdaIntegration)
+    );
 
-    const productById = products.addResource('{product_id}');
-    const productByIdLambdaIntegration = new lambda.Function(this, 'Single Product Endpoint', {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'products.getProductById',
-    });
-    productById.addMethod(lambda.HttpMethod.GET, new apigw.LambdaIntegration(productByIdLambdaIntegration));
+    const productById = products.addResource("{product_id}");
+    const productByIdLambdaIntegration = new lambda.Function(
+      this,
+      "Single Product Endpoint",
+      {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        code: lambda.Code.fromAsset("lib/products"),
+        handler: "lambda.getProductById",
+      }
+    );
+    productById.addMethod(
+      lambda.HttpMethod.GET,
+      new apigw.LambdaIntegration(productByIdLambdaIntegration)
+    );
   }
 }
