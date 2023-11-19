5882269963197115e9f5f62c6910133aa63c82ec
Iryna Kavalenka <iryna_kavalenka@epam.com>
2023-11-19 16:56:10 +0100

    update mocks

--- lambda/products/mocks/products.mock.ts
+++ lambda/products/mocks/products.mock.ts
@@ -2,40 +2,46 @@ import { AvailableProduct, Product } from "../models/product.model";
 
 export const products: Product[] = [
   {
-    description: "Short Product Description1",
-    id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
-    price: 24,
-    title: "ProductOne",
+    description:
+      "Like all Kenyan coffees, our Kiringa has a clean, fruity flavour and a subtle sweetness.",
+    id: "kenya-kiringa",
+    price: 20,
+    title: "Kenya Kiringa Coffee",
   },
   {
-    description: "Short Product Description7",
-    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
+    description:
+      "Drip Coffee Blend is a mix of the highest quality beans from different parts of the world. ",
+    id: "drip-blend",
+    price: 11,
+    title: "Drip Coffee Blend",
+  },
+  {
+    description:
+      "The beans of this coffee are a carefully selected blend of Arabica beans from the Colombian Cauca region. ",
+    id: "decaf-colombia-cauca",
+    price: 12,
+    title: "Decaf Colombia Cauca Espresso",
+  },
+  {
+    description:
+      "Delicious autumnal flavours and aromas wrapped up in a coffee bag - pleasantly tart red currant reminiscent of summer, juicy pomegranate and lots of sweetness tasting of honey and milk chocolate.",
+    id: "fall-espresso",
+    price: 14,
+    title: "Fall Espresso",
+  },
+  {
+    description:
+      "More beans from Ethiopia - another temptation for the senses...",
+    id: "ethiopia-yirga-beloya",
     price: 15,
-    title: "ProductTitle",
+    title: "Ethiopia Yirga Beloya",
   },
   {
-    description: "Short Product Description2",
-    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
-    price: 23,
-    title: "Product",
-  },
-  {
-    description: "Short Product Description4",
-    id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
-    price: 15,
-    title: "ProductTest",
-  },
-  {
-    description: "Short Product Descriptio1",
-    id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
-    price: 23,
-    title: "Product2",
-  },
-  {
-    description: "Short Product Description7",
-    id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
-    price: 15,
-    title: "ProductName",
+    description:
+      "Meet our latest filter COSTA RICA coffee from the Las Lajas farm, which comes from a semi-washed process, which is an intermediate method between washed and natural processing.",
+    id: "costa-rica-las-lajas",
+    price: 16,
+    title: "Costa Rica Las Lajas",
   },
 ];
 
