43cf939e7142afa0659a59223464b2b72c98c5d4
Iryna Kavalenka <iryna_kavalenka@epam.com>
2023-11-26 22:47:24 +0100

    refactor

--- lib/products/lambda/utils/validate-body.util.ts
+++ lib/products/lambda/utils/validate-body.util.ts
@@ -6,6 +6,9 @@ export function isValidBody(body: any): boolean {
   if (typeof body === 'string') {
     try {
       body = JSON.parse(body);
+      if (!body || typeof body !== 'object') {
+        return false;
+      }
       return true;
     } catch(error) {
       return false;
