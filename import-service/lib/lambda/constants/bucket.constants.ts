f2bc564ce21fc488c789957adaa99e9c8f2a7d67
Iryna Kavalenka <iryna_kavalenka@epam.com>
2023-12-03 23:16:41 +0100

    refactor

--- import-service/lib/lambda/constants/bucket.constants.ts
+++ import-service/lib/lambda/constants/bucket.constants.ts
@@ -2,5 +2,6 @@ export const BUCKET_NAME: string = 'rss-import-service-bucket';
 export const BUCKET_ARN: string = 'arn:aws:s3:::rss-import-service-bucket';
 
 export enum Folders {
-  UPLOADED = 'uploaded'
+  UPLOADED = 'uploaded',
+  PARSED = 'parsed',
 }
